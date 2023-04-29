a69a38c9dbb64668f5574d898d934307e81d3a66
IamHARSHDABAS <iamharshdabas@gmail.com>
2023-04-29 12:57:11 +0530

    FIX: speed

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
-let drum = document.querySelectorAll('.drum');
-
-for (let i = 0; i < drum.length; i++) {
-  drum[i].addEventListener('keypress', function (event) {
-    switch (event.key) {
-      case 'w':
-        let audio_w = new Audio('sounds/tom-0.mp3');
-        audio_w.play();
-        break;
-      case 'a':
-        let audio_a = new Audio('sounds/tom-1.mp3');
-        audio_a.play();
-        break;
-      case 's':
-        let audio_s = new Audio('sounds/tom-2.mp3');
-        audio_s.play();
-        break;
-      case 'd':
-        let audio_d = new Audio('sounds/tom-3.mp3');
-        audio_d.play();
-        break;
-      case 'j':
-        let audio_j = new Audio('sounds/tom-4.mp3');
-        audio_j.play();
-        break;
-      case 'k':
-        let audio_k = new Audio('sounds/tom-5.mp3');
-        audio_k.play();
-        break;
-      case 'l':
-        let audio_l = new Audio('sounds/tom-6.mp3');
-        audio_l.play();
-        break;
-      default:
-        break;
-    }
-  })
-}
+document.addEventListener('keypress', function (event) {
+  const audio_w = new Audio('sounds/tom-0.mp3');
+  const audio_a = new Audio('sounds/tom-1.mp3');
+  const audio_s = new Audio('sounds/tom-2.mp3');
+  const audio_d = new Audio('sounds/tom-3.mp3');
+  const audio_j = new Audio('sounds/tom-4.mp3');
+  const audio_k = new Audio('sounds/tom-5.mp3');
+  const audio_l = new Audio('sounds/tom-6.mp3');
+  switch (event.key) {
+    case 'w':
+      audio_w.play();
+      break;
+    case 'a':
+      audio_a.play();
+      break;
+    case 's':
+      audio_s.play();
+      break;
+    case 'd':
+      audio_d.play();
+      break;
+    case 'j':
+      audio_j.play();
+      break;
+    case 'k':
+      audio_k.play();
+      break;
+    case 'l':
+      audio_l.play();
+      break;
+    default:
+      break;
+  }
+})
